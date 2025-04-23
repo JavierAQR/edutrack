ff7afdabfd8ef91d6b5a1a0524007f5117f6031e
NavaDark <jhoynavarro20@gmail.com>
2025-04-22 23:42:51 -0500

    Create StudentSectionRepository.java

---
+++ src/main/java/com/edutrack/repositories/StudentSectionRepository.java
@@ -0,0 +1,7 @@
+package com.edutrack.repositories;
+
+import com.edutrack.entities.StudentSection;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentSectionRepository extends JpaRepository<StudentSection, Long> {
+}
