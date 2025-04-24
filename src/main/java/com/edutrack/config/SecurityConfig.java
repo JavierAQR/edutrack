248ac01bab07d78d2ff74fe64cfc27a44b26dcb3
Angel kbro <angelgaraycocheacarhuas@hotmail.com>
2025-04-24 18:06:09 -0500

    AcademicPeriod

--- src/main/java/com/edutrack/config/SecurityConfig.java
+++ src/main/java/com/edutrack/config/SecurityConfig.java
@@ -18,6 +18,7 @@ public class SecurityConfig {
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/api/users/**").permitAll()
                 .requestMatchers("/api/institutions/**").permitAll()
+                .requestMatchers("/api/periods/**").permitAll()
                 // .anyRequest().authenticated()
             );
             // .httpBasic(Customizer.withDefaults());
